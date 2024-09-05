4e97e231f99e194577015ef4a5689af57118ce6c
katya havriuk <0303gavkat@gmail.com>
2024-09-04 19:39:42 -0600

    sign in button

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -1,17 +1,32 @@
-import { auth } from '@/server/auth'
-import { UserButton } from './user-button'
+import { auth } from '@/server/auth';
+import { UserButton } from './user-button';
+import { Button } from '../ui/button';
+import { LogIn } from 'lucide-react';
+import Link from 'next/link';
 
 export default async function Nav() {
-  const session = await auth()
-  console.log(session)
+  const session = await auth();
   return (
-    <header className='py-4'>
+    <header className="py-8">
       <nav>
-        <ul className='flex justify-between'>
-          <li>logo</li>
-          <li><UserButton expires={session?.expires} user={session?.user}/></li>
+        <ul className="flex justify-between">
+          <li>Logo</li>
+          {!session ? (
+            <li>
+              <Button asChild>
+                <Link className='flex gap-2' href="/auth/login">
+                    <LogIn size={16} />
+                    <span>Login</span>
+                </Link>
+              </Button>
+            </li>
+          ) : (
+            <li>
+              <UserButton expires={session?.expires} user={session?.user} />
+            </li>
+          )}
         </ul>
       </nav>
     </header>
-  )
+  );
 }
